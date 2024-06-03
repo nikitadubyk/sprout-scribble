ea70b1a3670f042c761bd313cced882138442c94
Nikita Dubyk <nikita_dubyk@mail.ru>
2024-06-03 09:25:19 +0300

    feat: remove post requests and schema

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,7 @@
-import { Button } from "@/components/ui/button";
-import getPosts from "@/server/actions/get-posts";
-import createPost from "@/server/actions/create-post";
-
 export default async function Home() {
-  const { error, success } = await getPosts();
-
-  if (error) {
-    return <div>error</div>;
-  }
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Main page</p>
-      {success?.map((value) => (
-        <p key={value.id}>{value.title}</p>
-      ))}
-
-      <form action={createPost}>
-        <input type="text" name="title" placeholder="text" />
-
-        <Button type="submit">submit</Button>
-      </form>
     </main>
   );
 }
--- server/schemas/posts.ts
+++
@@ -1,6 +0,0 @@
-import { text, pgTable, serial } from "drizzle-orm/pg-core";
-
-export const posts = pgTable("posts", {
-  id: serial("id").primaryKey(),
-  title: text("title").notNull(),
-});
